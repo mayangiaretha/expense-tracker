bd9fea9d89646f310a80ffe9aa41fc7ab5ab7249
mayangiaretha <myaretha2620@gmail.com>
2022-05-12 20:59:46 +0300

    tracker app

---
+++ src/components/AddTransaction.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import {useState} from 'react'
+
+function AddTransaction() {
+    const [text, setText] = useState('')
+    const [amount, setAmout] = useState('0')
+
+  return (
+    <>
+      <h3>Add new transaction</h3>
+      <form>
+        <div className="form-control">
+          <label htmlFor="text">Text</label>
+          <input type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder="Enter text..." />
+        </div>
+        <div className="form-control">
+          <label for="amount"
+            >Amount <br />
+            (negative - expense, positive - income)</label
+          >
+          <input type="number" value={amount} onChange={(e) => setAmout(e.target.value)}  placeholder="Enter amount..." />
+        </div>
+        <button className="btn">Add transaction</button>
+      </form>
+    </>
+  )
+}
+
+export default AddTransaction
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+
+const Header = () => {
+  return (
+    <h2>
+        EXPENSE TRACKER
+    </h2>
+  )
+}
+
+export default Header
