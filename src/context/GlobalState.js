90cdc45758822fb7a95e7ae645b1b26cbb979b4e
mayangiaretha <myaretha2620@gmail.com>
2022-05-18 20:48:57 +0300

    add and remove expenses

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -1,18 +1,34 @@
 import { createContext, useReducer } from 'react';
+import { appReducer } from './AppReducer';
 
 const initialState = {
-    transactions:[
-        // const dummyTransactions = [
- { id: 1, text: 'Flower', amount: -20 },
- { id: 2, text: 'Salary', amount: 300 },
- { id: 3, text: 'Book', amount: -10 },
- { id: 4, text: 'Camera', amount: 150 }
-]
+    transactions:[]
     
 }
 export const GlobalContext = createContext(initialState)
 
 export const GlobalProvider = ({ children }) => {
-    const [state, dispatch] = useReducer();
+    const [state, dispatch] = useReducer(appReducer, initialState);
+
+    function deleteTransaction(id) {
+        dispatch({
+            type: 'DELETE_TRANSACTION',
+            payload: id
+        })
+    }
+    function addTransaction(transaction) {
+        dispatch({
+            type: 'ADD_TRANSACTION',
+            payload: transaction 
+        })
+    }
+
+    return(<GlobalContext.Provider value={{
+        transactions: state.transactions, 
+        deleteTransaction,
+        addTransaction
+    }}>
+        {children}
+    </GlobalContext.Provider>)
 
 }
